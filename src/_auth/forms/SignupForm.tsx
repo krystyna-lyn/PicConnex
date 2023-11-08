e65770117ef6ac583acad9b647871e12cdc45346
krystyna-lyn <67439674+krystyna-lyn@users.noreply.github.com>
2023-11-08 21:53:20 +0000

    update

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -5,8 +5,13 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Input } from "@/components/ui/input"
 import { SingupValidation } from "@/lib/validation/validation"
 import { z } from "zod"
+import Loader from "@/components/shared/Loader"
+import { Link } from "react-router-dom"
 
 const SignupForm = () => {
+let email: string;
+
+  const isLoading = false;
 
   // 1. Define your form.
   const form = useForm<z.infer<typeof SingupValidation>>({
@@ -17,36 +22,86 @@ const SignupForm = () => {
   })
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SingupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
+  async function onSubmit(values: z.infer<typeof SingupValidation>) {
+    console.log("Submitting values", values)
     console.log(values)
+    //const new User = await createUserAccount(values);
   }
 
   return (
     <Form {...form}>
       <div>
-        <img src="/assets/images/logo.svg"/>
+        <img src="/assets/images/logo.svg" />
+        <h2 className="h3-boold md:h2-bold p-5 sm: pt-12"> Create new account</h2>
+        <p className="text-light-3 small-medium md:base-regular">To use PicConnex enter your details</p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {
+              isLoading ?
+                <div className="flex-center gap-2">Loading... <Loader /></div>
+                : 'Sign up'
+            }
+
+          </Button>
+          <p className="text-light-2 small-medium md:base-regular">
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log in</Link>
+          </p>
+        </form>
       </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
     </Form>
 
   )
